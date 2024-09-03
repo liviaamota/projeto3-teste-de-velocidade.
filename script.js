1f12acb7c26f9cddded706dfd5129c0d667e6a98
liviaamota <livia.mota11@aluno.ce.gov.br>
2024-09-03 05:47:06 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+let vehicle1 = prompt('Digite o nome do primeiro veículo ')
+let speed1 = prompt('Digite a velocidade do primeiro veículo ')
+let vehicle2 = prompt('Digite o nome do segundo veículo ')
+let speed2 = prompt('Digite a velocidade do segundo veículo ')
+if (speed1>speed2) {
+    alert(`O ${vehicle1} está a ${speed1}km/h, e está mais rápido que o ${vehicle2} que está a ${speed2}km/h`)
+   
+} else if (speed1<speed2)
+        alert(`O ${vehicle2} está a ${speed2}km/h, e está mais rápido que o ${vehicle1} que está a ${speed1}km/h.`);
+    
+else {(speed1===speed2)
+alert(`Tanto ${vehicle1} e ${vehicle2} estão na mesma velocidade de ${speed1}km/h`)
+
+     }let vehicle1 = prompt('Digite o nome do primeiro veículo ')
+let speed1 = prompt('Digite a velocidade do primeiro veículo ')
+let vehicle2 = prompt('Digite o nome do segundo veículo ')
+let speed2 = prompt('Digite a velocidade do segundo veículo ')
+if (speed1>speed2) {
+    alert(`O ${vehicle1} está a ${speed1}km/h, e está mais rápido que o ${vehicle2} que está a ${speed2}km/h`)
+   
+} else if (speed1<speed2)
+        alert(`O ${vehicle2} está a ${speed2}km/h, e está mais rápido que o ${vehicle1} que está a ${speed1}km/h.`);
+    
+else {(speed1===speed2)
+alert(`Tanto ${vehicle1} e ${vehicle2} estão na mesma velocidade de ${speed1}km/h`)
+
+     }
